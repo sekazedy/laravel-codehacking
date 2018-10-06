a289e8198df383559f8f29489b19e7d71e69db52
Sergey Chernishenko <sekazedy@gmail.com>
2018-10-06 14:33:03 +0300

    #20 - small adds to kernel and updated composer.lock

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\Admin::class,
